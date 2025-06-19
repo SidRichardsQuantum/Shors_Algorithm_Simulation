9632f6f546c120aa62b6b0f72aaf539dc4ec1385
Sid Richards <siddrichards@hotmail.co.uk>
2025-06-19 21:25:22 +0100

    r via maxima

--- src/plots_and_period/find_period.py
+++ src/plots_and_period/find_period.py
@@ -10,18 +10,28 @@ def find_period(N, a, sparse=True):
 
     n_qubits = ceil(log2(N))
     M = 2 ** n_qubits
-    
-    # Calculate mean probability
-    prob_first_register = compute_probs(N, a, sparse)  # Just compute, don't plot
-    mean_prob = np.mean(prob_first_register)
-    
-    # Find indices where probability is above the mean
-    above_mean_indices = np.where(prob_first_register >= mean_prob)[0]
 
-    # Use the differences between above-mean states to estimate period candidates
-    s = []  # Period candidates list
-    for i in range(1, len(above_mean_indices)):
-        s.append(M * i / (above_mean_indices[i] - above_mean_indices[0]))
-    r = round(np.mean(s))  # Average of the candidates, as an integer
+    # Find the max probability
+    prob_first_register = compute_probs(N, a, sparse)  # Just compute, don't plot
+    max_prob = np.max(prob_first_register)
+
+    # Find states with max probability
+    max_indices = np.where(prob_first_register == max_prob)[0]
+
+    # Use the difference in indices between the first two states with the maximum probability
+    r = round(M / (max_indices[1] - max_indices[0]))
+    
+    # # Calculate mean probability
+    # prob_first_register = compute_probs(N, a, sparse)  # Just compute, don't plot
+    # mean_prob = np.mean(prob_first_register)
+    
+    # # Find indices where probability is above the mean
+    # above_mean_indices = np.where(prob_first_register >= mean_prob)[0]
+
+    # # Use the differences between above-mean states to estimate period candidates
+    # s = []  # Period candidates list
+    # for i in range(1, len(above_mean_indices)):
+    #     s.append(M * i / (above_mean_indices[i] - above_mean_indices[0]))
+    # r = round(np.mean(s))  # Average of the candidates, as an integer
     
     return r, prob_first_register  # Return both period and probabilities
